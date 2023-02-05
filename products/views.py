42b5c63b8b6117b4248f4b29059d38617d5d9b4f
Egor Ostroumov <araimo@yandex.ru>
2023-02-05 12:40:54 +0300

    base templates

--- products/views.py
+++ products/views.py
@@ -7,6 +7,13 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'products/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = {
+            'title': 'django Store',
+            'is_promotion': False,
+        }
+        return context
+
 
 def products_view(request):
     return render(request, 'products/products.html')

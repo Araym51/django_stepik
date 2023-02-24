e476514c899a639ca4fd9816d78df15cfca28ac5
Egor Ostroumov <araimo@yandex.ru>
2023-02-24 20:53:16 +0300

    register and user profile

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib import auth
 from django.urls import reverse
 
 from users.models import User
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -37,7 +37,18 @@ def registration(request):
     return render(request, 'users/register.html', context)
 
 
-# class UserLoginView(LoginView):
-#     title = 'Авторизация'
-#     form_class = UserLoginForm
-#     template_name = 'users/login.html'
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {
+        'title': 'Store - профиль',
+        'form': form
+    }
+    return render(request, 'users/profile.html', context)

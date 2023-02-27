73e5dca5a7a0553323028bae74ac0f51ff9aa887
Egor_Ostroumov <araimo@yandex.ru>
2023-02-27 10:20:16 +0300

    basket methods

--- users/views.py
+++ users/views.py
@@ -48,10 +48,11 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
+
     context = {
         'title': 'Store - профиль',
         'form': form,
-        'baskets': Basket.objects.filter(user=request.user)
+        'baskets': Basket.objects.filter(user=request.user),
     }
     return render(request, 'users/profile.html', context)
 

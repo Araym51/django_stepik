2ddec0d5356319c7518cd9e6ae7b0c3eeea3e43d
Egor_Ostroumov <araimo@yandex.ru>
2023-02-27 10:33:39 +0300

    access decorator

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.views.generic import TemplateView, ListView
+from django.contrib.auth.decorators import login_required
+
 from .models import ProductCategory, Product, Basket
 from users.models import User
 # Create your views here.
@@ -36,6 +38,7 @@ class ProductsView(ListView):
         return context
 
 
+@login_required
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
@@ -50,6 +53,7 @@ def basket_add(request, product_id):
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
+@login_required
 def basket_remove(request, basket_id):
     basket = Basket.objects.get(id=basket_id)
     basket.delete()
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.views import LoginView
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from products.models import Basket
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -38,6 +39,7 @@ def registration(request):
     return render(request, 'users/register.html', context)
 
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)

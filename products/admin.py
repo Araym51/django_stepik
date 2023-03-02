fc6da9abf4148fd114207f89d87f0f2f54022810
Egor_Ostroumov <araimo@yandex.ru>
2023-03-02 20:08:23 +0300

    admin

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from products.models import ProductCategory, Product, Basket
 # Register your models here.
 
-admin.site.register(Product)
 admin.site.register(ProductCategory)
-admin.site.register(Basket)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ( 'image', ('name', 'category'),  'description', ('price', 'quantity'))
+    readonly_fields = ('name',)
+    search_fields = ('name',)
+    ordering = ('price',)  # ('-price',) сортировка в обратном порядке
+
+
+# для TabularInline не используется декоратор админа
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0

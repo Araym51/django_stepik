e78e3b4114f0a0ad59907f333273fbbf55a9acef
Egor Ostroumov <araimo@yandex.ru>
2023-03-04 16:29:02 +0300

    user register and profile CBV

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from users.views import login, registration, profile, logout
+from django.contrib.auth.decorators import login_required
+
+from users.views import login, logout, UserRegistrationView, UserProfileView  # registration, profile,
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('register/', registration, name='register'),
-    path('profile/', profile, name='profile'),
+    # path('register/', registration, name='register'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('profile/<int:pk>', login_required(UserProfileView.as_view()), name='profile'),
+    # path('profile/', profile, name='profile'),
     path('logout/', logout, name='logout'),
 ]

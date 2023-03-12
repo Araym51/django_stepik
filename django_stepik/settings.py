98c65f88646826fb7e7e25377aa1da0aef1746f7
Egor Ostroumov <araimo@yandex.ru>
2023-03-12 16:31:14 +0300

    postgre migration

--- django_stepik/settings.py
+++ django_stepik/settings.py
@@ -80,12 +80,25 @@ WSGI_APPLICATION = 'django_stepik.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'store_db',
+        'USER': 'araym',
+        'PASSWORD': 'xjx213vr',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation

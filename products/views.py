114bfbec6c0a22c56a023388d9ae080f97657289
Egor Ostroumov <araimo@yandex.ru>
2023-03-22 11:02:13 +0300

    isort

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.core.cache import cache
 from django.shortcuts import HttpResponseRedirect, render
 from django.views.generic import ListView, TemplateView
-from django.core.cache import cache
 
 from common.views import TitleMixin
 

eccf23366ef515e413cbe8e23d21e330761635c5
Egor Ostroumov <araimo@yandex.ru>
2023-03-26 10:28:16 +0300

    order form and model

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.edit import CreateView
 
+from orders.form import OrderForm
 # Create your views here.
 
 class OrderCreateView(CreateView):
     template_name = 'orders/order-create.html'
+    form_class = OrderForm
